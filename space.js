884b5b6b253baf45f50c78bbc69292b98d09f469
Gummala Likhihta <likhithagummala@gmail.com>
2024-12-27 21:22:48 +0530

    genai-Space-Explorer

---
+++ space.js
@@ -0,0 +1,18 @@
+function exploreMore() {
+    // alert("Get ready to explore the universe!");
+    window.open('explore.html');
+}
+function createStars() {
+    const container = document.querySelector("body");
+    for (let i = 0; i < 1000; i++) {
+      // Increase the number of stars to 1000
+      const star = document.createElement("div");
+      star.className = "star";
+      star.style.width = ".1px";
+      star.style.height = ".1px";
+      star.style.top = Math.random() * 100 + "%";
+      star.style.left = Math.random() * 100 + "%";
+      container.appendChild(star);
+    }
+  }
+  createStars();
